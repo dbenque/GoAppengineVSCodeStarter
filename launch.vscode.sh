f8408871da8725db8f7bc120cb598954b52cb6fc
David Benque <dbenque@gmail.com>
2016-06-08 21:32:49 +0200

    fix sed option and search good binary for vscode

--- launch.vscode.sh
+++ launch.vscode.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-#in order to support several visual-studio-code installation and alias, if $VSCODEBIN define no overwrite it.
-if [ "$VSCODEBIN" = "" ]
-then
-   VSCODEBIN="code"
-fi
-
 # Check that the basefolder of the environment is correctly set
 if [ ! -d "$BASEFOLDER" ]; then
     echo "Please define the BASEFOLDER variable. It should point to the parent folder of your Appengine and Golang environment."
@@ -36,7 +30,24 @@ fi
 GOPATH=$BASEFOLDER/go:$GOROOT
 PATH=$GOAPPENGINE:$GOPATH/bin:$PATH
 
-sed -ie 's|"go.gopath".*".*"|"go.gopath": "'"$GOPATH"'"|g' .vscode/settings.json
-sed -ie 's|"go.goroot".*".*"|"go.goroot": "'"$GOROOT"'"|g' .vscode/settings.json
+sed -i -e 's|"go.gopath".*".*"|"go.gopath": "'"$GOPATH"'"|g' .vscode/settings.json
+sed -i -e 's|"go.goroot".*".*"|"go.goroot": "'"$GOROOT"'"|g' .vscode/settings.json
 
-eval $VSCODEBIN ./ &
+# Launch vscode
+CMD='echo "visual studio code binary not installed or not in PATH."'
+which code &> /dev/null
+if [ $? -eq 0 ]; then
+   CMD="code ./"
+fi
+
+which vscode &> /dev/null
+if [ $? -eq 0 ]; then
+   CMD="vscode ./"
+fi
+
+which visual-studio-code &> /dev/null
+if [ $? -eq 0 ]; then
+   CMD="visual-studio-code ./"
+fi
+
+$CMD
